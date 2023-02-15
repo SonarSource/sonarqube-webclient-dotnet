c9cb47a9c9132a058c9568861d020d399c024fee
georgii-borovinskikh-sonarsource <georgii.borovinskikh@sonarsource.com>
2023-02-15 14:10:43 +0100

    Deserialize CreationDate for TaintRaised

---
+++ SonarQube.Client/Helpers/MillisecondUnixTimestampDateTimeOffsetConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SonarQube.Client.Helpers
+{
+    public class MillisecondUnixTimestampDateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+    {
+        public override void WriteJson(JsonWriter writer,
+            DateTimeOffset value,
+            JsonSerializer serializer)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override DateTimeOffset ReadJson(JsonReader reader,
+            Type objectType,
+            DateTimeOffset existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            var timestamp = (long?)reader?.Value;
+
+            return timestamp.HasValue 
+                ? DateTimeOffset.FromUnixTimeMilliseconds(timestamp.Value) 
+                : default(DateTimeOffset);
+        }
+    }
+}
